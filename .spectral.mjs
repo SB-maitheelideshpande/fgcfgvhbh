d8cd0738e4f88bdc2267675d5f5e6e1dd227c9a7
maitheeli.deshpande@smartbear.com <maitheeli.deshpande@smartbear.com>
2025-08-12 12:30:29 +0530

    Added 86 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://x-20209.stoplight-dev.com/api/v1/projects/cHJqOjEyMTg/spectral.js?branch_slug=main&token=c3782e5f-94c1-400f-ba93-81b0c06d5a15";
+export default { extends: ruleset };
---
+++ docs/loss-workflow.md
@@ -0,0 +1,7 @@
+---
+stoplight-id: g675wh0xt8wcr
+---
+
+# Core workflow in Synergy Studio
+
+![synergystudio_loss-workflow.png](synergystudio_loss-workflow.png)
